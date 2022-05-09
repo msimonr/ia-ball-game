36f0dea3cc8e00d7d5f85a52d19d7a4d3164546a
msimonr <mauricio.simon.r@gmail.com>
2022-05-08 23:41:39 -0300

    Updates

---
+++ precache-manifest.be08d76b21fa2ceedbacccc29ea0c1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6bc0a6d25d9c6954d96fb67c908207",
+    "url": "/ia-ball-game/index.html"
+  },
+  {
+    "revision": "f92741496751f0338615",
+    "url": "/ia-ball-game/static/css/main.a4687642.chunk.css"
+  },
+  {
+    "revision": "5a6cf25cceea19c47a4d",
+    "url": "/ia-ball-game/static/js/2.fd8b20c2.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ia-ball-game/static/js/2.fd8b20c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92741496751f0338615",
+    "url": "/ia-ball-game/static/js/main.77128ac4.chunk.js"
+  },
+  {
+    "revision": "aee02b0fc641670c02d1",
+    "url": "/ia-ball-game/static/js/runtime-main.3f4525d6.js"
+  }
+]);
